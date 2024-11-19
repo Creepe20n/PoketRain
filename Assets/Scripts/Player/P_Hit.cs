43b3a90bc3c88fc3a0daf2630a621bac13ceb360
Cat Labs <91755609+Creepe20n@users.noreply.github.com>
2024-11-19 17:18:37 +0100

    To new Project

--- Assets/Scripts/Player/P_Hit.cs
+++
@@ -1,7 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface P_Hit {
-    public void hitPlayer(int addScorePoints = 0,float changeSpeed = 0,int changeLife = 0,GameManager gM = null);
-}
